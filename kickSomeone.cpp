40cc129d1417f7b32902e19294e61442781526d5
Miyuki Ito <mitou.1221@gmail.com>
2025-01-27 15:28:39 +0200

    added more one solution to find channel name but still work in progress

--- kickSomeone.cpp
+++ kickSomeone.cpp
@@ -5,6 +5,8 @@ void Server::kickSomeone(int cdf, std::string arg) {
     std::vector<std::string> tokens;
     std::string token;
 
+
+    // prints out for testing, delete them 
     std::cout << "Hello from kickSomeone function." << std::endl;
     std::cout << "cdf is '" << cdf << "' and arg is '" << arg << "'." << std::endl;
 
@@ -13,7 +15,26 @@ void Server::kickSomeone(int cdf, std::string arg) {
         tokens.push_back(token);
     }
 
+    // prints out for testing, delete them
     for (const std::string& token : tokens) {
         std::cout << "Token: " << token << std::endl;
     }
+
+    // we might not needed this becaus irss says ""
+    if (tokens.size() < 3) {
+        std::cerr << "At least 3 tokens needed to execute KICK" << std::endl;
+    }
+
+    // std::vector<std::string>::iterator end = tokens.end();
+    // for (std::vector<std::string>::iterator begin = tokens.begin();  begin != end; ++begin) {
+    //     if (begin != this->_clients.end())
+    // }
+
+    if (_channels.find(channelName) == _channels.end()) {
+        std::cerr << "Channel '" << channelName << "' does not exist." << std::endl;
+        return;
+    }
+    this->_channels
+
+
 }
